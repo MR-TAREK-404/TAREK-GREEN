61af6fa5c415cfc9a76b778f9b2e91d7746535c9
MR-TAREK-404 <tarek713609@gmail.com>
2024-06-15 23:07:27 +0600

    Update TAREK-GREEN.py

--- TAREK-GREEN.py
+++ TAREK-GREEN.py
@@ -4,6 +4,6 @@
 import platform,os,time
 bit = platform.architecture()[0]
 if bit == '64bit':
-    import MR_TAREK_BIT_64
+    import GREEN_64
 elif bit == '32bit':
-    import MR_TAREK_BIT_32
+    import GREEN_32
